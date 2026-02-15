7078ffe8bb38cf8875912a91f87a5eda0b9fb3ad
xszs <2457331233@qq.com>
2026-02-15 11:56:00 +0800

    feat:twikoo评论系统api设置

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -6,7 +6,7 @@ export const commentConfig: CommentConfig = {
 
 	//twikoo评论系统配置
 	twikoo: {
-		envId: "http://47.115.218.89:8080/",
+		envId: "https://blog.tjausbj.com.cn/twikooapi",
 		// 设置 Twikoo 评论系统语言
 		lang: "zh-CN",
 		// 是否启用文章访问量统计功能
